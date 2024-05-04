e4cb36d37229d99f6717eef35a069693d0bf6bf1
Thanh Ma <thanhm963@gmail.com>
2024-05-04 13:49:19 +0700

    Implementing the use state and use effect hooks

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL:'https://9c96-103-106-239-104.ap.ngrok.io',
+    baseURL:'https://1a78-116-100-46-140.ngrok-free.app',
     headers:{"ngrok-skip-browser-warning": "true"}
-})
+});
